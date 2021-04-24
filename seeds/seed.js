307c9a18f71862c8195b2024ee0295bc779c00b4
Kristen Picard <kristenpicard93@gmail.com>
2021-04-24 17:10:15 -0400

    Working on seeds

---
+++ seeds/seed.js
@@ -0,0 +1,15 @@
+const sequelize = require("../config/connection");
+const seedPost = require("./postData");
+const seedUser = require("./userData");
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedPost();
+
+  await seedUser();
+
+  process.exit(0);
+};
+
+seedAll();
